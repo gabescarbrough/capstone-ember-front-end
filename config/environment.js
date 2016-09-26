5c826d06621232f2966acd8e115c9584e52943bb
gabescarbrough <gabescarbrough@gmail.com>
2016-09-26 12:11:07 -0400

    Replace app name
    Replace app name in template to match back-end

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'capstone-project',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
