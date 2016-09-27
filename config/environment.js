29eb9a23bc5a35e97f514e2f2452b91d1083bfa6
gabescarbrough <gabescarbrough@gmail.com>
2016-09-27 16:23:09 -0400

    Add ENV and apiHost info to adapter
    Allows front-end to communicate with back-end

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    apiHost: 'http://localhost:3000',
     modulePrefix: 'capstone-project',
     environment: environment,
     baseURL: '/',
